cb0ffe96d155ad2a7697ad9327924c0cc4d3a33b
25Dec <thiennhannguyenhuynh@gmail.com>
2022-10-19 10:13:44 +0700

    exercise 1-1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import "./index.css";
+import MyComponent from "./MyComponent";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-	<p>
-		Hello, <strong>JSX</strong>
-	</p>
-);
+root.render(<MyComponent />);
