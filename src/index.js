a127375a25567d09abce885b293aec3569fbb6c2
25Dec <thiennhannguyenhuynh@gmail.com>
2022-10-25 21:19:08 +0700

    exercise4.2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import "./index.css";
+import { Button } from "antd";
+import "antd/dist/antd.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-	<p>
-		Hello, <strong>JSX</strong>
-	</p>
+const handleClick = () => {
+	alert("Alert!!!");
+};
+const element = (
+	<Button style={{ margin: "10px 10px" }} type="primary" onClick={handleClick}>
+		Alert me!
+	</Button>
 );
+ReactDOM.createRoot(document.getElementById("root")).render(element);
